f5dfc2adcdb7003c2f6b92168250c342982fd79c
itachi-237 <dedendy237@googlemail.com>
2025-08-11 12:31:34 +0200

    02_tables erstellt

---
+++ 02_tables/02_tables.sql
@@ -0,0 +1,41 @@
+\! cls
+
+-- Datenbank anzeigen
+SHOW DATABASES;
+
+-- Datenbank anlegen, falls noch nicht existent
+CREATE DATABASE IF NOT EXISTS boo;
+
+-- Tabellen anzeigen
+USE boo;
+
+-- Tabelle löschen, falls existent
+-- Nur bei Test
+DROP TABLE IF EXISTS kunde;
+
+-- Tabelle anlegen
+CREATE TABLE IF NOT EXISTS kunde 
+(
+    kunden_id INT AUTO_INCREMENT PRIMARY KEY,
+    kundenname VARCHAR(45),
+    kundenalter TINYINT,
+    kunden_email UNIQUE VARCHAR(100)
+);
+
+-- Tabelle anzeigen
+SHOW TABLES;
+
+-- Struktur anzeigen
+DESCRIBE kunde;
+
+-- Datensätze / Zeilen
+INSERT INTO kunde(kunden_id, kundenname, kundenalter) VALUES (DEFAULT,"Meier",68); 
+INSERT INTO kunde(kunden_id, kundenname, kundenalter) VALUES (DEFAULT,"Gadaffi",89); 
+INSERT INTO kunde(kunden_id, kundenname, kundenalter) VALUES (DEFAULT,"Putin",59);
+INSERT INTO kunde(kunden_id, kundenname, kundenalter) VALUES (DEFAULT,"Putin",64);
+INSERT INTO kunde(kunden_id, kundenname, kundenalter) VALUES (DEFAULT,"Jong-Un",38);
+INSERT INTO kunde(kundenname, kundenalter) VALUES ("Putin",64);
+
+
+-- Tabellendaten (Inhalte) anzeigen
+SELECT * FROM kunde;
