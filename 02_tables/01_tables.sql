d1f74a2cd7f8bcc82272e368aea9561a04deb3d2
itachi-237 <dedendy237@googlemail.com>
2025-08-11 10:58:53 +0200

    update 2 tables

--- 02_tables/01_tables.sql
+++ 02_tables/01_tables.sql
@@ -3,8 +3,25 @@
 -- Datenbank anzeigen
 SHOW DATABASES;
 
--- Datenbank anlegen
-CREATE DATABASE boo;
+-- Datenbank anlegen, falls noch nicht existent
+CREATE DATABASE IF NOT EXISTS boo;
 
--- Datenbank anzeigen
-SHOW DATABASES;
+-- Tabellen anzeigen
+USE boo;
+
+-- Tabelle löschen, falls existent
+-- Nur bei Test
+DROP TABLE IF EXISTS kunde;
+
+-- Tabelle anlegen
+CREATE TABLE IF NOT EXISTS kunde 
+(
+    k_name VARCHAR(45),
+    k_alter TINYINT
+);
+
+-- Tabelle anzeigen
+SHOW TABLES;
+
+-- Struktur anzeigen
+DESCRIBE kunde;
